6a6effada4db114d22d17b267498d97a9d1bc428
Mystery00 <mystery0dyl520@gmail.com>
2017-03-06 13:28:01 +0800

    标签调试成功

---
+++ app/src/main/java/com/mystery0/hitokoto/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.mystery0.hitokoto;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+public class SettingsFragment extends PreferenceFragment
+{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.perferences);
+    }
+}
