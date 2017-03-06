e20e00e037af8f931441d91b0b839cb2e6c1d552
Mystery00 <mystery0dyl520@gmail.com>
2017-03-06 14:26:53 +0800

    尝试添加监听

--- app/src/main/java/com/mystery0/hitokoto/SettingsFragment.java
+++ app/src/main/java/com/mystery0/hitokoto/SettingsFragment.java
@@ -3,9 +3,12 @@ package com.mystery0.hitokoto;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class SettingsFragment extends PreferenceFragment
 {
+    private static final String TAG = "SettingsFragment";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
     {
