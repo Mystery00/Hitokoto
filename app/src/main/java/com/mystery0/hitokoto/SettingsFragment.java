676cbe95de0865e9a68325b194df9ead03652c80
Mystery00 <mystery0dyl520@gmail.com>
2017-03-06 14:50:49 +0800

    添加监听

--- app/src/main/java/com/mystery0/hitokoto/SettingsFragment.java
+++ app/src/main/java/com/mystery0/hitokoto/SettingsFragment.java
@@ -1,18 +1,54 @@
 package com.mystery0.hitokoto;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.support.annotation.Nullable;
 import android.util.Log;
-import android.view.MenuItem;
 
 public class SettingsFragment extends PreferenceFragment
 {
     private static final String TAG = "SettingsFragment";
+    private Preference refreshNow;
+    private Preference showCurrent;
+    private Preference testSource;
+    private Preference chooseSource;
+    private Preference clickToRefresh;
+    private Preference setRefreshTime;
+    private Preference setTextColor;
+    private Preference textBold;
+    private Preference textAligned;
+    private Preference textSize;
+    private Preference notShowSource;
+    private Preference resourceAddress;
+    private Preference about;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        initialized();
+        monitor();
+    }
+
+    private void initialized()
+    {
         addPreferencesFromResource(R.xml.perferences);
+        setHasOptionsMenu(true);
+
+        refreshNow = findPreference(getString(R.string.key_refresh_now));
+        showCurrent = findPreference(getString(R.string.key_show_current));
+    }
+
+    private void monitor()
+    {
+        refreshNow.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+        {
+            @Override
+            public boolean onPreferenceClick(Preference preference)
+            {
+                Log.i(TAG, "onPreferenceClick: " + preference.getTitle());
+                return false;
+            }
+        });
     }
 }
