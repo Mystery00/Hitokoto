e69fa44f06bbbb4c188484d785cf07cad2f732bf
Mystery00 <mystery0dyl520@gmail.com>
2017-03-10 14:24:57 +0800

    修复小部件一个接收错误，修复int解析为id值，List修改为Set

--- app/src/main/java/com/mystery0/hitokoto/widget/HitokotoWidget.java
+++ app/src/main/java/com/mystery0/hitokoto/widget/HitokotoWidget.java
@@ -5,25 +5,23 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.v4.content.ContextCompat;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
-import com.mystery0.hitokoto.App;
 import com.mystery0.hitokoto.Hitokoto;
 import com.mystery0.hitokoto.R;
 import com.mystery0.hitokoto.SettingsActivity;
 import com.mystery0.tools.Logs.Logs;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HitokotoWidget extends AppWidgetProvider
 {
     private static final String TAG = "HitokotoWidget";
-    private static List<Integer> idsSet = new ArrayList<>();
+    private static Set<Integer> idsSet = new HashSet<>();
 
     static void updateAllAppWidget(String text, Context context, AppWidgetManager appWidgetManager)
     {
@@ -50,6 +48,7 @@ public class HitokotoWidget extends AppWidgetProvider
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
     {
+        Logs.i(TAG, "onUpdate: ");
         for (int id : appWidgetIds)
         {
             idsSet.add(id);
@@ -61,6 +60,7 @@ public class HitokotoWidget extends AppWidgetProvider
     @Override
     public void onDeleted(Context context, int[] appWidgetIds)
     {
+        Logs.i(TAG, "onDeleted: ");
         for (int id : appWidgetIds)
         {
             idsSet.remove(id);
@@ -94,8 +94,11 @@ public class HitokotoWidget extends AppWidgetProvider
         if ("android.appwidget.action.APPWIDGET_UPDATE".equals(action))
         {
             Hitokoto hitokoto = (Hitokoto) intent.getSerializableExtra(context.getString(R.string.hitokoto_object));
-            Logs.i(TAG, "onReceive: " + hitokoto.getHitokoto());
-            Logs.i(TAG, "onReceive: " + hitokoto.getFrom());
+            if (hitokoto != null)
+            {
+                Logs.i(TAG, "onReceive: " + hitokoto.getHitokoto());
+                Logs.i(TAG, "onReceive: " + hitokoto.getFrom());
+            }
         }
     }
 }
