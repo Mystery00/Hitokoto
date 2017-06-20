ab74bcc742ca66fd5a1fb1bc589c855e27e6d0fd
Mystery00 <mystery0dyl520@gmail.com>
2017-06-20 13:58:57 +0800

    修改主分支

--- app/src/main/java/com/mystery0/hitokoto/ShortCutsActivity.java
+++ app/src/main/java/com/mystery0/hitokoto/ShortCutsActivity.java
@@ -10,20 +10,20 @@ import com.mystery0.hitokoto.widget.WidgetConfigure;
 
 public class ShortCutsActivity extends AppCompatActivity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        if (WidgetConfigure.getEnable())
-        {
-            startService(new Intent(App.getContext(), OnClickService.class));
-            Toast.makeText(App.getContext(), R.string.hint_broadcast, Toast.LENGTH_SHORT)
-                    .show();
-        } else
-        {
-            Toast.makeText(App.getContext(), R.string.hint_add_widget, Toast.LENGTH_SHORT)
-                    .show();
-        }
-        finish();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		if (WidgetConfigure.getEnable())
+		{
+			startService(new Intent(App.getContext(), OnClickService.class));
+			Toast.makeText(App.getContext(), R.string.hint_broadcast, Toast.LENGTH_SHORT)
+					.show();
+		} else
+		{
+			Toast.makeText(App.getContext(), R.string.hint_add_widget, Toast.LENGTH_SHORT)
+					.show();
+		}
+		finish();
+	}
 }
